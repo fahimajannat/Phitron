c8438d043792653fe338427ee092e74f38cd6005
fahimajannat <fahimajannati652627@gmail.com>
2024-10-12 23:05:22 +0600

    pattern2.c

--- Semister 01/Week 02/Module 06/pattern2.c
+++ Semister 01/Week 02/Module 06/pattern2.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main(){
     for(int i=1;i<=5;i++){
-        for(int j=1; j<=i; j++){
-         printf("* ");  
+        for(int j=i; j>=1; j--){
+         printf("%d ",j);  
         }
         printf("\n");
 
     }
 }
 /*output
-* 
-* * 
-* * * 
-* * * * 
-* * * * * 
-
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1 
 */
