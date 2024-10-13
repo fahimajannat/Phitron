fdb0f28d9e75b1075b3d771b00af88df3337409e
fahimajannat <fahimajannati652627@gmail.com>
2024-10-13 17:19:19 +0600

    next_alphadet.c

---
+++ Semister 01/Week 02/practice/naxt_alphadet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    if (ch == 'z')
+    {
+        printf("a");
+       
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+         printf("%c", ch + 1);
+    }
+    return 0;
+}
