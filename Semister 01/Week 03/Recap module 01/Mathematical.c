182f347b0cc18f1fcfc6de3dc65263cf20b48383
Fahima Islam Jannati <fahimajannati652627@gmail.com>
2024-10-18 13:15:07 +0600

    Add files via upload

---
+++ Semister 01/Week 03/Recap module 01/Mathematical.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+    int A,B,C;
+    char S,Q;
+    int sum=0;
+    int sub=0;
+    int maltiplication=0;
+    scanf("%d %c %d %c %d",&A,&S,&B,&Q,&C);
+    if(S == '+' && Q == '=' ){
+        if( A + B == C ){
+            printf("Yes\n");
+        }
+        else{
+            sum=A+B;
+            printf("%d",sum);
+
+        }
+    }
+        else if(S =='-' && Q =='='){
+            if(A-B==C){
+                printf("Yes\n");
+            }
+            else{
+                sub=A-B;
+                printf("%d",sub);
+            }
+        }
+        else{
+            if(A*B==C){
+                printf("Yes\n");
+            }
+            else{
+                maltiplication=A*B;
+                printf("%d",maltiplication);
+
+            }
+        }
+        
+
+        
+    }
+
