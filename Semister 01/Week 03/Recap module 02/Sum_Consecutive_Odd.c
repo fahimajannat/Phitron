6ed85e6d85b0f1bc33210aa8ef15e9ac4572832b
fahimajannat <fahimajannati652627@gmail.com>
2024-10-19 22:26:38 +0600

    add

---
+++ Semister 01/Week 03/Recap module 02/Sum_Consecutive_Odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int cs = 0; cs<t; cs++){
+        int x,y;
+        scanf("%d %d",&x,&y);
+        int sum=0;
+        if (x>y){
+            int temp=x;
+            x=y;
+            y=temp;
+        }
+        for( int i=x+1; i<y; i++){
+            if(i%2 != 0){
+                sum+=i;
+            }
+        }
+
+
+            printf("%d\n",sum);
+  
+    }
+
+    
+}
